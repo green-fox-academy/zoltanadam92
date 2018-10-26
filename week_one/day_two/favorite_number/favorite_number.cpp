e7eb8095089d0354a838581e7b55bb7f62831c9e
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-26 15:43:09 +0200

    day_two git commit -mday_two

---
+++ week_one/day_two/favorite_number/favorite_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+int mynumber = 8;
+    std::cout << "What is your favorite number?" << std::endl;
+    std::cin >> mynumber;
+    std::cout << "My favorite number is:" << mynumber << std::endl;
+    
+    return 0;
+}
