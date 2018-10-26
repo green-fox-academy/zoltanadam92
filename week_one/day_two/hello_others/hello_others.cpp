72fefab7242b4fbd22be2eb1dda12ca9329357dd
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-26 15:09:57 +0200

    day_two git commit -mday_two

---
+++ week_one/day_two/hello_others/hello_others.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello Peti!" << std::endl;
+    std::cout << "Hello Dani!" << std::endl;
+    std::cout << "Hello Mark!" << std::endl;
+    return 0;
+}
