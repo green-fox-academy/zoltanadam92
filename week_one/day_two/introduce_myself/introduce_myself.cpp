987cd532a20f0ea101531510c73209d012e32805
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-26 15:15:03 +0200

    day_two git commit -mday_two

---
+++ week_one/day_two/introduce_myself/introduce_myself.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Prints a string to the terminal window.
+    std::cout << "Zoltan Banffy-Pinter" << std::endl;
+
+    // Prints an integer to the terminal window.
+    std::cout << 26 << std::endl;
+
+    // Prints a floating point number to the terminal window.
+    std::cout << 1.92 << std::endl;
+
+    return 0;
+}
