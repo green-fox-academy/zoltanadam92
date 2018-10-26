28bc90ccd1eb1d078fc4fc33458fdf138b5ffcaf
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-26 15:57:47 +0200

    day_two git commit -mday_two

---
+++ week_one/day_two/bmi/bmi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+    std::cout << 81.2 / (1.78*1.78) << std::endl;
+    // Print the Body mass index (BMI) based on these values
+
+    return 0;
+}
