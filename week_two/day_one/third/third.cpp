72abbd1aedaf406402fefa6e4a1e71cf99992ff8
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-29 16:37:03 +0100

    day_one git commit -mday_one

---
+++ week_two/day_one/third/third.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+int q[4] = {4,5,6,7};
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+    std::cout << q[2] <<std::endl;
+    return 0;
+}
