5c1cd14c14ee64c318b6a2574d968764f32f810d
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-30 13:10:49 +0100

    day_two git commit -mday_two

---
+++ week_two/day_two/urlfixer/urlfix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string url("https://www.reddit.com/r/nevertellmetheodds");
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+    std::cout << url << std::endl;
+
+    return 0;
+}
