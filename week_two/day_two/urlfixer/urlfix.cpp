df83e8e5c1eca330184c72abf63c2d78de04fbd5
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-10-30 13:33:31 +0100

    day_two

--- week_two/day_two/urlfixer/urlfix.cpp
+++ week_two/day_two/urlfixer/urlfix.cpp
@@ -3,11 +3,18 @@
 
 int main(int argc, char* args[])
 {
-    std::string url("https://www.reddit.com/r/nevertellmetheodds");
+    std::string url("https://www.reddit.com/r/nevertellmethebots");
+    std::string from ="bots";
 
+    {
+        int start_pos = url.find(from);
+        if (start_pos == std::string::npos)
+            std::cout << "Cannot replace" << std::endl;
+        else
+            url.replace(start_pos, from.length(), "odds");
+    }
     // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
     // Also, the URL is missing a crucial component, find out what it is and insert it too!
-
     std::cout << url << std::endl;
 
     return 0;
