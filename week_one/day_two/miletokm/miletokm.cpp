decf5698655b34b43b8b50053d6e3c79af271089
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-11-04 17:15:30 +0100

    day_two

---
+++ week_one/day_two/miletokm/miletokm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+int distance;
+std::cout<< "Please give me a distance in km";
+std::cin >> distance;
+std::cout<<"That would be " << distance*0.6 << " in miles" << std::endl;
+
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+
+    return 0;
+}
