9b735e30db7e2f5e6ffbe14323c01eba136f7bc6
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-11-12 13:23:39 +0100

    day_one

---
+++ week_four/day_one/print_each_line/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    // Write a program that opens a file called "my-file.txt", then prints
+    // each of lines form the file.
+    // You have to create the file, you can use C-programming but it is not mandatory
+    ifstream theFile("my_file.txt");
+    string text;
+    while (theFile >> text){
+        cout << text << endl;
+}
+
+return 0;
+}
