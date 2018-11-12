25684104d052a20a8dc76d0c78b26c94eb7d7e1d
Bánffy-Pintér Zoltán <bpzoltan92@gmail.com>
2018-11-12 14:57:37 +0100

    day_one

--- week_four/day_one/write_single_line/main.cpp
+++ week_four/day_one/write_single_line/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 ofstream filename("task4.txt");
 
-filename << "This is the text i want to insert into my new txt file.";
+filename << "Bánffy-Pintér Zoltán";
 filename.close();
     return 0;
 }
